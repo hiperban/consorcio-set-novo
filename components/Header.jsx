9d45cc303d90ac4cf3b94194c37a7ff494f2e420
hiperban <dr-caldeira@hotmail.com>
2025-09-27 18:09:06 -0300

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,8 +12,15 @@ export default function Header(){
     <header className="bg-white/90 backdrop-blur border-b">
       <div className="container py-4 flex items-center justify-between">
         <Link href="/" className="flex items-center gap-2">
-          <Image src="/logo.svg" width={120} height={28} alt="Hiperban"/>
-        </Link>
+  <Image
+    src="/hiperban-logo.png"
+    alt="Hiperban"
+    width={140}   // ajuste fino do tamanho
+    height={32}   // mantenha a proporção da sua imagem
+    priority      // carrega mais rápido no topo
+  />
+</Link>
+
         <nav className="flex gap-4 text-sm">
           <Link className={linkCls('/')} href="/">Simulador</Link>
           <Link className={linkCls('/compare')} href="/compare">Comparar</Link>
